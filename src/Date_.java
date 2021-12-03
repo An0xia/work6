8c7ef1bfe7fa95664d8473b788baa09e58f8a006
syclover <syclover@yeah.net>
2021-12-03 16:36:10 +0800

    Initial commit

---
+++ src/Date_.java
@@ -0,0 +1,9 @@
+public class Date_ {
+    public static void main(String[] args){
+        java.util.Date date new =new java.util.Date();
+        for(long i= 10000;i<1e10;i*=10){
+            date.setTime(i);
+            System.out.println(date.toString());
+        }
+    }
+}
